bf93ba4e7e5a42da4fd45cb160fc206b086e1025
nikhgarg <nikhgarg93@gmail.com>
2015-11-19 11:04:31 -0800

    made more robust in passing in variables/agent types/locations.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ components:
 """
 import csv
 import numpy as np
-from agent import Agent, Agent_BasicLearning, Agent_BasicProportionalLearning
+from agent import Agent, Agent_BasicLearning, Agent_BasicProportionalLearning, Agent_StubbornLTE, Agent_StubbornWiFi
 import environment
 from environment import BS
 import random
@@ -43,19 +43,36 @@ def getNetworkGeometry(CASE):
     BSs = []
     UElocs = []
     if CASE is 0:
-        BSs = [BS((-1, 0), 'LTE'), BS((1, 0), 'WiFi')]
         UElocs = [(0,0)]
     if CASE is 1:
         BSs = [BS((-1, 0), 'LTE'), BS((1, 0), 'WiFi')]
         UElocs = [(0,0), (0,0)]
     return [BSs, UElocs]
+    
+AgentNameDictionary = {'Agent' : Agent, 'BasicLearning' : Agent_BasicLearning, 'BasicProportionalLearning': Agent_BasicProportionalLearning, 'StubbornLTE' : Agent_StubbornLTE, 'StubbornWiFi' : Agent_StubbornWiFi}
 
-def createAgents(CASE, UElocs, BSs):
+def createAgents(variables):
     Agents= []
+    BSs = [BS((-1, 0), 'LTE'), BS((1, 0), 'WiFi')] #hardcode for now fine.
     actspace = range(0, len(BSs))
-    for i in range(0,len(UElocs)):
-        Agents.append(Agent_BasicProportionalLearning(UElocs[i], actspace))
-    return Agents
+    for i in range(0,variables['NumAgents']):
+        xlocstring = 'Agent'+str(i)+'X'
+        ylocstring = 'Agent'+str(i)+'Y'
+        AgentTypeString = 'Agent'+str(i)+'Type'
+        UEloc = [0,0]
+        if variables['AgentLocs'] == 'Fixed' and xlocstring in list(variables.keys()):
+            UEloc[0] = variables[xlocstring];
+            UEloc[1] = variables[ylocstring]
+        else:
+            UEloc[0] = 2*random.random() - 1
+            UEloc[1] = 2*random.random() - 1
+        Ag = None
+        if AgentTypeString in list(variables.keys()):
+            Ag = AgentNameDictionary[variables[AgentTypeString]](UEloc, actspace)
+        else:
+            Ag = random.choice(list(AgentNameDictionary.values()))(UEloc, actspace)      
+        Agents.append(Ag)
+    return [Agents, BSs]
   
 def determineWhichBSTransmitting(BSs, variables, t=-1, t_cutoff=-1):
     x = [0]*len(BSs)
@@ -91,26 +108,27 @@ def determineRewards(BSs, Agents, actions, variables, t=-1, t_cutoff=-1):
     return rewards
 
 for configuration in csv_file:
-    variables = configuration;
+    variables = configuration.copy();
     if variables['VALID'] == '0':
         continue
-    for var in variables:
-        if var != 'ExperimentName':
+    categoricalvariables = ['ExperimentName', 'Agent1Type', 'Agent0Type', 'AgentLocs']
+    for var in configuration:
+        if len(variables[var])>0 and (var not in categoricalvariables):
             variables[var]= float(variables[var])
-        else:
-            variables[var] = variables[var] + '_' + str(time.time());
-    CASE = int(variables['CASE'])
+        elif len(variables[var]) ==0:
+            del variables[var]
+        variables['ExperimentTime'] = str(time.time())
     COEXISTENCEPROTOCOL = int(variables['COEXISTENCEPROTOCOL'])        
     NumExperiments = int(variables['NumRepeat']);
+    variables['NumAgents'] = int(variables['NumAgents']);
+
     AgentRewards = []
     AgentActions = []
 
     for experiment_num in range(0, NumExperiments):
         if experiment_num % 1000 == 1:
                 print(experiment_num) 
-        [BSs, UElocs] = getNetworkGeometry(CASE);
-        Agents = createAgents(CASE, UElocs, BSs)
-        
+        [Agents, BSs] = createAgents(variables)
         t = 0
         while (True):
         #    ask each agent for its play
@@ -154,24 +172,32 @@ for configuration in csv_file:
 
     #output results:
     
-    fieldnamesorder = ['ExperimentName', 'CASE', 'COEXISTENCEPROTOCOL', 'Agent_type', 'Agent_location']
+    fieldnamesorder = ['ExperimentName', 'ExperimentTime', 'NumAgents', 'COEXISTENCEPROTOCOL', 'Agent_type', 'Agent_location']
     AgentPrintingDicts = []
     for i in range(0, len(Agents)):
         AgentPrintingDicts.append(variables.copy())
         AgentPrintingDicts[i]['Agent_location'] = Agents[i].location #NOTE Only valid for deterministic locations
         AgentPrintingDicts[i]['Agent_type'] = type(Agents[i]).__name__ #NOTE Only valid for deterministic locations/orderings
 
-        
+    NotToPrint = ['Agent0Type', 'Agent1Type', 'Agent0X', 'Agent1X', 'Agent0Y', 'Agent1Y', 'AgentLocs']
+    for name in NotToPrint:
+        if name in variables:
+            del variables[name]
     for var in variables:
         if var not in fieldnamesorder:
             fieldnamesorder.append(var)
+            
     for i in range(0, int(variables['T_cutoff'])):
         fieldnamesorder.append(str(i))
         for agent in range(0, len(Agents)):
             AgentPrintingDicts[agent][str(i)] = AgentRewards[agent][i]
-    
-    outputfilehandle = open(output_file, 'a')
-    outputwriter = csv.DictWriter(outputfilehandle, fieldnames = fieldnamesorder, delimiter=',', quotechar='"', extrasaction='raise')
+    for i in range(0, int(variables['T_cutoff'])):
+        fieldnamesorder.append("Action_" + str(i))
+        for agent in range(0, len(Agents)):
+            AgentPrintingDicts[agent]["Action_" + str(i)] = AgentActions[agent][i]    
+    outputfilehandle = open(output_file, 'a', newline='')
+    outputwriter = csv.DictWriter(outputfilehandle, fieldnames = fieldnamesorder, delimiter=',', quotechar='"', extrasaction='ignore')
+   
     outputwriter.writeheader();
     for i in range(0, len(Agents)):
         outputwriter.writerow(AgentPrintingDicts[i])
